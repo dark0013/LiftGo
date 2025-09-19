b955766965fce5b2d53fa5ce2a9f2e360389e67a
DARKROSS <andresarcangel00@gmail.com>
2025-09-19 00:35:54 -0500

    add log

--- src/main/java/com/darkorss/LiftGo/controller/ClientsController.java
+++ src/main/java/com/darkorss/LiftGo/controller/ClientsController.java
@@ -11,18 +11,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.OK;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("/v1/clients")
 @AllArgsConstructor
 public class ClientsController {
+    private static final Logger logger = LoggerFactory.getLogger(ClientsController.class);
     @Autowired
     private final IClientsService clientsService;
     private final IClientMapper clientMapper;
 
     @GetMapping("/{mail}")
     public ResponseEntity<ClientsDto>  getClient(@PathVariable String mail) {
-        ClientsDto client = clientMapper.toDto(clientsService.findByEmail(mail)); // clientsService.findByEmail(mail);
+        logger.info("Buscando cliente con email: {}", mail);
+        ClientsDto client = clientMapper.toDto(clientsService.findByEmail(mail));
+        logger.debug("Cliente encontrado: {}", client);
         return new ResponseEntity<>(client, OK);
     }
 
